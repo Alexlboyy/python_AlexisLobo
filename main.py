1e92d71c22dac8f48e378d71f2366d89d38c639c
Alexis Lobo <alexislbo2005@gmail.com>
2025-11-17 15:27:45 +0100

    first

---
+++ main.py
@@ -0,0 +1,17 @@
+import socket
+import platform #Permet d'obtenir des informations sur le systeme d'explotation
+
+hostname = socket.gethostname()
+adresse_ip = socket.gethostbyname(hostname)
+py_ver = platform.python_version()
+proc = platform.processor()
+
+print("================INFORMATION================")
+print("Le nom de la machine :" + " " + hostname)
+print("============================================")
+print("L'adresse IP de la machine : " + adresse_ip)
+print("============================================")
+print("La version du Python : " + "Python " + py_ver)
+print("============================================")
+print("Votre processeur : " + proc)
+print("============================================")
